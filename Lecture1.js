3948be2af05a4c412ec9ac25d19a7a0d245001c2
AsimSheraz12 <1240100471@uskt.edu.pk>
2026-08-31 12:36:29 +0500

    Add new file basics of Java Scritp

---
+++ Lecture1.js
@@ -0,0 +1,62 @@
+/* console.log("Hello World");
+console.log("My Name is Asim Sheraz and Love to Learn Java Script Languge"); */
+
+/* fullname = "Asim Sheraz";
+console.log(fullname);
+
+age = 20;
+
+console.log(age);
+
+price = 22.99;
+
+console.log(price); */ 
+
+// X = null;
+// Y = undefined;
+
+// /* console.log(X);
+// console.log(Y); */ 
+
+// console.log(X,Y);
+
+
+ /*isfollow = true;
+
+console.log(isfollow);
+
+
+isfollow1 = false;
+
+console.log(isfollow1); */
+
+
+
+// let a = 10;
+
+// //console.log(a);
+
+// typeof a;
+
+
+const Student = {
+    
+    fullName : "Asim Sheraz",
+    age : 20,
+    cgpa : 3.45,
+    isPass : true
+
+}
+
+// console.log(Student);
+
+// console.log(typeof Student);
+
+
+// console.log(Student.fullName);
+
+Student["age"] = Student["age"] + 1;
+
+console.log(Student["age"]);
+
+
