67e205e83110e74fbfb8f539db7658584deaeeee
AsimSheraz12 <1240100471@uskt.edu.pk>
2026-08-31 12:52:35 +0500

    Add 1st Lecture Project File

---
+++ Project1.js
@@ -0,0 +1,14 @@
+const Profile = {
+
+    profileTitle : "Asim Sheraz",
+    isFollow : false,
+    isMessage : true,
+    posts : 195,
+    followers : 569000,
+    following : 4,
+    fullname : "Asim Sheraz",
+    linkedinlink : true,
+    descrition : "Co-Founder of Foxx Enterprisess"
+};
+
+console.log(Profile);
